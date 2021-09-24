3dc2a3373a338e4ecfd76c6a2aa8cc343f2ed2fd
renckbryce <renckbryce@gmail.com>
2021-09-24 13:33:49 -0600

    Started new follower script

---
+++ Assets/Final/ChaosMask/Scripts/TargetFollower.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    public Transform Target {set {target = value;}}
+
+    [SerializeField]
+    private Vector3 offset;
+
+    [SerializeField]
+    private float dampingTime;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        MoveTowardTarget();   
+    }
+
+    private void MoveTowardTarget()
+    {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, dampingTime);
+    }
+}

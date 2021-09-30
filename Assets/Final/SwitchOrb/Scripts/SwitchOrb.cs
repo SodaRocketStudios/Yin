eb9228dfae6b81a5138b8b66c6de5911b081ce16
renckbryce <renckbryce@gmail.com>
2021-09-30 13:51:29 -0600

    orbs now act like a switch

--- Assets/Final/SwitchOrb/Scripts/SwitchOrb.cs
+++ Assets/Final/SwitchOrb/Scripts/SwitchOrb.cs
@@ -5,12 +5,15 @@ public class SwitchOrb : MonoBehaviour
     private WorldMaskMover maskMover;
 
     [SerializeField]
-    private float moveDistance;
+    private float orderTarget;
+
+    [SerializeField]
+    private float chaosTarget;
 
     [SerializeField]
     private float timeToTarget;
 
-    private bool hasBeenTriggered;
+    private bool useChaosTarget = true;
 
     private void Awake()
     {
@@ -21,8 +24,10 @@ public class SwitchOrb : MonoBehaviour
     {
         if(other.CompareTag("Player") == true)
         {
+            float moveDistance = useChaosTarget?chaosTarget:orderTarget;
             maskMover.SetTarget(transform.position + Vector3.right*moveDistance, Vector3.zero, timeToTarget);
-            hasBeenTriggered = true;
+            
+            useChaosTarget = !useChaosTarget;
         }
     }
 }
